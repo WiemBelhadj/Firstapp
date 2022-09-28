8b62ba1c628b462f351bed56c6b7411d86175ad0
WiemBelhadj <wiem.belhadg@orthlane.com>
2022-09-28 10:05:01 +0100

    commited

--- src/app/member-form/member-form.component.ts
+++ src/app/member-form/member-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Member } from '../modals/Member';
 import { MemberService } from '../service/member.service';
 
 @Component({
@@ -11,6 +12,8 @@ import { MemberService } from '../service/member.service';
 export class MemberFormComponent implements OnInit {
 form:any;
 currentID:any;
+item1:any;
+itemglobale:any;
   // injection de dépendances ::  constructor(MemberService : MemberService) { }
   constructor(private MemberService : MemberService, private router:Router, private activatedRoute :ActivatedRoute) { }
 
@@ -26,13 +29,25 @@ currentID:any;
     this.currentID=this.activatedRoute.snapshot.params.id;
     //etape 2 : si id a une valeur => declancher la fct du serv
     if(!!this.currentID){
-      this.MemberService.getMemberById(this.currentID).then(()=>{});
+      this.MemberService.getMemberById(this.currentID).then((item)=>{
+        this.itemglobale=item;
+        this.initForm1(item);
+      });
     }
     //getMemberById(id) => member
     //etape 3 : sinon appeler initForm()
     else this.initForm();
 
 
+  }
+
+  initForm1(item:Member): void {
+    this.form = new FormGroup({
+      cin : new FormControl(item.cin, Validators.required),
+      name : new FormControl(item.name, Validators.required),
+      cv : new FormControl(item.cv, Validators.required),
+      type : new FormControl(item.type, Validators.required)
+    });
   }
 
   initForm(): void {
